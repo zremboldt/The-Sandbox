bca1d1566a2fe2da213bdb4f095ce927c8b89ba5
Zac <zacaree@gmail.com>
2019-05-23 10:27:06 -0500

    Fixed a class name issue

--- ES6 Course_Wes Bos/009 this and arrow fns/script_02.js
+++ ES6 Course_Wes Bos/009 this and arrow fns/script_02.js
@@ -2,19 +2,19 @@
 
 // You generally want to use a standard fn as your top level fn to contain scope.
 
-const box = document.querySelector(".box");
+const box = document.querySelector('.box');
 
 // If we use a standard function and don't bind it to anything
 // then "this" will refer to the global context; Window in this case.
 // We can fix this by using an arrow fn inside the timeout.
-box.addEventListener("click", function() {
+box.addEventListener('click', function() {
   console.log(this);
   // setTimeout(function() {
   //   console.log(this);
-  //   this.classList.toggle("animate");
+  //   this.classList.toggle("grow");
   // }, 100);
   setTimeout(() => {
     console.log(this);
-    this.classList.toggle("animate");
+    this.classList.toggle('grow');
   }, 100);
 });
