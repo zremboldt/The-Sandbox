065c4d088389d5a8841b9038f02dc801702a5d7d
zacaree <zacharem@gmail.com>
2019-06-22 16:19:53 -0500

    Learning about instance and prototype members.

---
+++ Personal Experiments/OOP in JS/02 Prototypes/006_prototypeVsInstanceMembers.js
@@ -0,0 +1,52 @@
+function Circle(radius) {
+  this.radius = radius;
+  // this.draw = function() {
+  //   console.log(`Radius of ${this.radius}`);
+  // };
+}
+
+const c1 = new Circle(1);
+const c2 = new Circle(2);
+
+/////////////////////////////////////////////////////////////////
+
+// The constructor of c1 will be Circle, this is found in the prototype of c1.
+console.log(Object.getPrototypeOf(c1));
+
+/////////////////////////////////////////////////////////////////
+
+// GOLD NUGGET ↓
+// c1 and c2 both have an independent (copied) draw method taking up space in memory.
+// Let's imagine we had 1000 circles made from Circle.
+// They'd all have their own copy of draw().
+// A better way: We can take this draw method out of the Circle object and
+// put it in its prototype because we'll only have a single instance of
+// this prototype in memory so the same will be true of the draw method.
+// So how do we do this?
+
+// Circle.prototype gives us exactly the same thing as c1.__proto__
+// These two properties ↑ are referencing the same object in memory. That's the "Circle base".
+// So using Circle.prototype we can quickly and safely access this object.
+
+Circle.prototype.draw = () => console.log(`draw method added using prototype`);
+const c3 = new Circle(3);
+
+// Now log c3 and take a look at the prototype!
+// Because of prototypical inheritance we can still access the draw method like this
+// c3.draw()
+// because when JS engine doesn't find draw() on c3 it just keeps looking up the
+// prototype chain! Cool!
+
+// So we have two types of properties/methods in js
+// We have "Instance Properties/Methods" and "Prototype Properties/Methods"
+// Example ↓
+
+function Square(width) {
+  // Instance property
+  this.width = width;
+}
+
+// This will be a Prototype property
+Square.prototype.height = 4;
+
+// Paused lesson at the 3 minute mark...
