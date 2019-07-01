9421a4e18a32f1f99a4828837f7e1f5efc751aea
zacaree <zacharem@gmail.com>
2019-06-30 22:54:20 -0500

    Watching "High-Performance Images Using Gatsby"

--- Experiments/199 Birds/199-birds/src/components/Card.js
+++ Experiments/199 Birds/199-birds/src/components/Card.js
@@ -1,26 +1,46 @@
 import React from "react"
-import Image from "../components/image"
+// import Image from "../components/image"
+import Img from "gatsby-image"
 
-const Card = ({ name, img }) => {
+const Card = props => {
+  const { id } = props.edge.node
+  // console.log(props.edge.node.id)
   return (
     <div className="card">
       <div className="cardImg">
-        <Image />
+        {/* <Image /> */}
+        {/* <Img fluid={img ? img.childImageSharp.fluid : {}} alt={name} /> */}
+        {/* <Img fluid={id.fluid} backgroundColor={"#eeeeee"} /> */}
       </div>
-      <div className="info">
-        <p className="name">{name}</p>
-      </div>
+      <figcaption>
+        {/* <h3>{name}</h3> */}
+        {/* <Copy>{copy}</Copy> */}
+      </figcaption>
     </div>
   )
 }
 
+// const Card = ({ name, img }) => {
+//   return (
+//     <div className="card">
+//       <div className="cardImg">
+//         {/* <Image /> */}
+//         {/* <Img fluid={img ? img.childImageSharp.fluid : {}} alt={name} /> */}
+//         <Img fluid={img.fluid} backgroundColor={"#eeeeee"} />
+//       </div>
+//       <figcaption>
+//         <h3>{name}</h3>
+//         {/* <Copy>{copy}</Copy> */}
+//       </figcaption>
+//     </div>
+//   )
+// }
+
 export default Card
 
-{
-  /* <div
+/* <div
         className="image"
         // style={{ backgroundImage: `url(../images/${img})` }}
       >
         <Image />
       </div> */
-}
--- Experiments/199 Birds/199-birds/src/pages/index.js
+++ Experiments/199 Birds/199-birds/src/pages/index.js
@@ -7,45 +7,69 @@ import Card from "../components/Card"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 
-const birds = [
-  {
-    name: "Mandarin Duck",
-    img: "IMG_8278.JPG",
-    video: "https://youtu.be/3oYbCpxwECQ",
-  },
-  {
-    name: "Flamingo",
-    img: "IMG_8275.JPG",
-    video: "https://youtu.be/3oYbCpxwECQ",
-  },
-  {
-    name: "Eurasian Kingfisher",
-    img: "IMG_8276.JPG",
-    video: "https://youtu.be/3oYbCpxwECQ",
-  },
-  {
-    name: "Mallard Duck",
-    img: "IMG_8277.JPG",
-    video: "https://youtu.be/3oYbCpxwECQ",
-  },
-]
-
-const IndexPage = () => (
-  <>
-    <SEO title="Home" />
-    <main className="main">
-      <h1 style={{ margin: "1rem 0" }}>199 Birds</h1>
-      <div className="grid">
+const IndexPage = ({ data }) => {
+  // const { edges } = data.allImageSharp
+  console.log(data)
+  return (
+    <>
+      <SEO title="Home" />
+      <main className="main">
+        <h1 style={{ margin: "1rem 0" }}>199 Birds</h1>
+        {/* {edges.map((edge, i) => (
+          <Card edge={edge} key={i} />
+        ))} */}
+        {/* <div className="grid">
         {birds.map((bird, i) => (
           <Card {...bird} key={i} />
         ))}
-      </div>
-    </main>
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      </div> */}
+      </main>
+      {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
-  </>
-)
+      {/* <Link to="/page-2/">Go to page 2</Link> */}
+    </>
+  )
+}
+
+export const query = graphql`
+  query ImageQuery {
+    allFile(filter: { sourceInstanceName: { eq: "images" } }) {
+      edges {
+        node {
+          relativePath
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              srcSet
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
+
+// const birds = [
+//   {
+//     name: "Mandarin Duck",
+//     img: "IMG_8278.JPG",
+//     video: "https://youtu.be/3oYbCpxwECQ",
+//   },
+//   {
+//     name: "Flamingo",
+//     img: "IMG_8275.JPG",
+//     video: "https://youtu.be/3oYbCpxwECQ",
+//   },
+//   {
+//     name: "Eurasian Kingfisher",
+//     img: "IMG_8276.JPG",
+//     video: "https://youtu.be/3oYbCpxwECQ",
+//   },
+//   {
+//     name: "Mallard Duck",
+//     img: "IMG_8277.JPG",
+//     video: "https://youtu.be/3oYbCpxwECQ",
+//   },
+// ]
