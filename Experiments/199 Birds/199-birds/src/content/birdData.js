e35666311b11d568642509c34f451543e879404e
zacaree <zacharem@gmail.com>
2019-07-20 15:57:43 -0500

    added bird types to the data sheet

--- Experiments/199 Birds/199-birds/src/content/birdData.js
+++ Experiments/199 Birds/199-birds/src/content/birdData.js
@@ -4,73 +4,89 @@ export const birdData = [
   ///////////////////////////////////////////
   {
     name: "Magpie",
+    type: "meadows",
     videoId: "Za9kK1IFX88",
     vidStart: "155",
   },
   {
     name: "Chaffinch",
+    type: "meadows",
     videoId: "qpg9noRrQek",
   },
   {
     name: "Wood Pigeon",
+    type: "meadows",
     videoId: "AWet9qFT15M",
     vidStart: "40",
   },
   {
     name: "Yellowhammer",
+    type: "meadows",
     videoId: "Wpu4mkzSh6A",
   },
   {
     name: "Bullfinch",
+    type: "meadows",
     videoId: "wrCdLXqBMOk",
   },
   {
     name: "Wren",
+    type: "meadows",
     videoId: "IvmVzZ3mjOE",
   },
   {
     name: "Sparrow",
+    type: "meadows",
     videoId: "PkOnpk8Wmow",
   },
   {
     name: "Bluetit",
+    type: "meadows",
     videoId: "seXURXJr0CY",
     vidStart: "7",
     vidStop: "12",
   },
   {
     name: "Long-Tailed Tit",
+    type: "meadows",
     videoId: "Tx8rJJfZ5Ak",
     vidStart: "36",
   },
   {
     name: "Whitethroat",
+    type: "meadows",
     videoId: "Tub7-_dLP7E",
   },
   {
     name: "European Greenfinch",
+    type: "meadows",
     videoId: "tVHuZ5ng3Gc",
   },
   {
     name: "European Robin",
+    type: "meadows",
     videoId: "SVNilOwikJ8",
   },
   {
     name: "Junco",
+    type: "meadows",
     videoId: "vlJUsAl4YCA",
   },
   {
     name: "Starling",
+    type: "meadows",
     videoId: "V4f_1_r80RY",
     vidStart: "33",
   },
   {
     name: "Collared Dove",
+    type: "meadows",
     videoId: "3Km-jtXueTw",
     vidStart: "10",
   },
   {
     name: "Blackbird",
+    type: "meadows",
     videoId: "DnClUsa1i_E",
   },
 
@@ -79,64 +95,78 @@ export const birdData = [
   ///////////////////////////////////////////
   {
     name: "Eurasian Wigeon",
+    type: "rivers",
     videoId: "MB3aZAoiPIg",
   },
   {
     name: "Mandarin Duck",
+    type: "rivers",
     videoId: "XtDu3dMsvXA",
   },
   {
     name: "Moorhen",
+    type: "rivers",
     videoId: "BtC-QorasAc",
     vidStart: "1",
   },
   {
     name: "Bittern",
+    type: "rivers",
     videoId: "ze1uzC7h8UE",
     vidStart: "8",
   },
   {
     name: "Flamingo",
+    type: "rivers",
     videoId: "QLV_K7DVeyU",
   },
   {
     name: "Malachite Kingfisher",
+    type: "rivers",
     videoId: "veQ2ilQrzMU",
     vidStart: "19",
   },
   {
     name: "Pintail",
+    type: "rivers",
     videoId: "gKU42FkOd2o",
   },
   {
     name: "Painted Stork",
+    type: "rivers",
     videoId: "bklSltY2XXc",
     vidStart: "32",
   },
   {
     name: "Mallard Duck",
+    type: "rivers",
     videoId: "C91s-TQlCPM",
     vidStart: "85",
   },
   {
     name: "Eurasian Kingfisher",
+    type: "rivers",
     videoId: "3zzJp9MQb3c",
   },
   {
     name: "Mute Swan",
+    type: "rivers",
     videoId: "TTGpMSXKE8A",
   },
   {
     name: "Heron",
+    type: "rivers",
     videoId: "_c24Tqx8SyQ",
   },
   {
     name: "Tufted Duck",
+    type: "rivers",
     videoId: "sIbMh-VEPrg",
     vidStart: "70",
   },
   {
     name: "Grebe",
+    type: "rivers",
     videoId: "ZbRrxw-H6xA",
   },
 
@@ -146,104 +176,126 @@ export const birdData = [
 
   {
     name: "Crested Auklet",
+    type: "sea",
     videoId: "QAHawigrH9w",
     vidStart: "34",
   },
   {
     name: "Ring-Billed Gull",
+    type: "sea",
     videoId: "6wrvr7MwCWA",
     vidStart: "102",
   },
   {
     name: "Rhinoceros Auklet",
+    type: "sea",
     videoId: "V4Oh3ZgST38",
     vidStart: "1",
   },
   {
     name: "Pigeon Guillemot",
+    type: "sea",
     videoId: "2IGzDdDSkdA",
     vidStart: "578",
   },
   {
     name: "Cormorant",
+    type: "sea",
     videoId: "UyC95DUDMqc",
   },
   {
     name: "Oystercatcher",
+    type: "sea",
     videoId: "wv0ukHz1WUk",
     vidStart: "49",
   },
   {
     name: "Red-Throated Diver",
+    type: "sea",
     videoId: "Mf7qfg0ljfo",
     vidStart: "10",
   },
   {
     name: "Tufted Puffin",
+    type: "sea",
     videoId: "8vQAQLnwZl8",
     vidStart: "31",
   },
   {
     name: "Common Tern",
+    type: "sea",
     videoId: "ruRUsa3Ls34",
   },
   {
     name: "Loon",
+    type: "sea",
     videoId: "U7CSWlNO9c4",
   },
   {
     name: "Blue-Footed Booby",
+    type: "sea",
     videoId: "z922by9_6Fw",
     vidStart: "8",
     vidStop: "178",
   },
   {
     name: "Pelican",
+    type: "sea",
     videoId: "BfEboMmwAMw",
     vidStart: "5",
   },
   {
     name: "European Herring Gull",
+    type: "sea",
     videoId: "Eqtx4e5CV-g",
     vidStart: "35",
   },
   {
     name: "Horned Puffin",
+    type: "sea",
     videoId: "zTOUcBPKRIQ",
   },
   {
     name: "Thick Billed Murre",
+    type: "sea",
     videoId: "hdKky3o6Gig",
   },
   {
     name: "Razorbill",
+    type: "sea",
     videoId: "7LuHNiC7fms",
     vidStart: "52",
   },
   {
     name: "Cassins Auklet",
+    type: "sea",
     videoId: "", // Can't find a good video for this...
   },
   {
     name: "Black Guillemot",
+    type: "sea",
     videoId: "7eClNZa8-Jw",
   },
   {
     name: "Shelduck",
+    type: "sea",
     videoId: "GcGe3iFQYhM",
     vidStart: "21",
   },
   {
     name: "Least Auklet",
+    type: "sea",
     videoId: "fWDWcIPWBkk",
   },
   {
     name: "Great Black-Backed Gull",
+    type: "sea",
     videoId: "6CL6NunnYkI",
     vidStart: "38",
   },
   {
     name: "Gannet",
+    type: "sea",
     videoId: "EwPrXOtBoVg",
     vidStart: "45",
   },
@@ -254,29 +306,35 @@ export const birdData = [
 
   {
     name: "Broad-Billed Hummingbird",
+    type: "desert",
     videoId: "04CwRLMwmEc",
     vidStart: "131",
   },
   {
     name: "Gilded Flicker",
+    type: "desert",
     videoId: "qSzk0nBS5Zc",
     vidStart: "10",
   },
   {
     name: "Elf Owl",
+    type: "desert",
     videoId: "h5MbKpNi3oY",
   },
   {
     name: "Cactus Pygmy Owl",
+    type: "desert",
     videoId: "fcL47Kiyuj0",
     vidStart: "90",
   },
   {
     name: "Roadrunner",
+    type: "desert",
     videoId: "U7nzSs3OBbg",
   },
   {
     name: "Gambel's Quail",
+    type: "desert",
     videoId: "Lzf0y84PbS4",
     vidStart: "9",
   },
@@ -287,30 +345,36 @@ export const birdData = [
 
   {
     name: "Noisy Miner",
+    type: "aussie",
     videoId: "602FT4hDJPE",
   },
   {
     name: "Kookaburra",
+    type: "aussie",
     videoId: "TZ9RyWz__6s",
     vidStart: "90",
   },
   {
     name: "Emu",
+    type: "aussie",
     videoId: "Aaq0Ynd8mjY",
     vidStart: "125",
   },
   {
     name: "Fruit Dove",
+    type: "aussie",
     videoId: "8ZDwuTag61w",
     vidStart: "20",
   },
   {
     name: "Magpie Goose",
+    type: "aussie",
     videoId: "lb8UZ2bFyrI",
     vidStart: "77",
   },
   {
     name: "Galah",
+    type: "aussie",
     videoId: "DH6NPcwxQqM",
     vidStart: "4",
   },
@@ -321,58 +385,72 @@ export const birdData = [
 
   {
     name: "Helmeted Guineafowl",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Saddle-Billed Stork",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Wattled Crane",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Red-Billed Hornbill",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Brown Quail",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Vulturine Guineafowl",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Ostrich",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Motmot",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Egyptian Goose",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Secretary Bird",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Short-Eared Owl",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Blue-Winged Parrotlet",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "Lilac-Breasted Roller",
+    type: "savanna",
     videoId: "",
   },
   {
     name: "European Roller",
+    type: "savanna",
     videoId: "",
   },
 
@@ -382,102 +460,127 @@ export const birdData = [
 
   {
     name: "Buzzard",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Atlantic Puffin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Albatross",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Blue-Eyed Shag",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Fulmar",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Snowy Owl",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Arctic Tern",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Eider",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Arctic Redpoll",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Snow Petrel",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Giant Petrel",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Little Auk",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Ptarmigan",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Magellanic Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Gentoo Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "King Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Adelie Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Macaroni Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Snares Crested Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "African Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Emperor Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Little Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Rockhopper Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Chinstrap Penguin",
+    type: "snow",
     videoId: "",
   },
   {
     name: "Humboldt Penguin",
+    type: "snow",
     videoId: "",
   },
 
@@ -487,90 +590,112 @@ export const birdData = [
 
   {
     name: "Raggiana Bird of Paradise",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Grey Parrot",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Crimson Topaz Hummingbird",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Moustached Babbler",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Broadbill",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Wilson's Bird of Paradise",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Quetzal",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Great Hornbill",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Harpy Eagle",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Hermit Hummingbird",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Red-Fan Parrot",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Toucan",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Oriental Dwarf Kingfisher",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Flowerpecker",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Blue-and-Yellow Macaw",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Hyacinth Macaw",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Red-and-Green Macaw",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Spectacled Owl",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "White Cockatoo",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Lory",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Mealy Amazon",
+    type: "rainforest",
     videoId: "",
   },
   {
     name: "Cassowary",
+    type: "rainforest",
     videoId: "AA2LkrR0_dw",
   },
 
@@ -580,90 +705,112 @@ export const birdData = [
 
   {
     name: "Green Woodpecker",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Crossbill",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Cuckoo",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Pheasant",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Wood Warbler",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Blue Jay",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Lewis' Woodpecker",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Long-Eared Owl",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Eurasian Crane",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Northern Goshawk",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Cardinal",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Tawny Owl",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Great Spotted Woodpecker",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Black Woodpecker",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Pileated Woodpecker",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Grouse",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Song Thrush",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Clark's Nutcracker",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "European Goldfinch",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Kea",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Sharp-Shinned Hawk",
+    type: "woodlands",
     videoId: "",
   },
   {
     name: "Mockingbird",
+    type: "woodlands",
     videoId: "",
   },
 
@@ -673,50 +820,62 @@ export const birdData = [
 
   {
     name: "Silver Campine Rooster",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Rouen Duck",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Embden Goose",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Indian Game Hen",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Barn Owl",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Bourbon Red Turkey",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Bronze Turkey",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Yokohama Rooster",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Welsummer Rooster",
+    type: "farm",
     videoId: "",
   },
   {
     name: "White Leghorn Rooster",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Silkie Hen",
+    type: "farm",
     videoId: "",
   },
   {
     name: "Gray Partridge",
+    type: "farm",
     videoId: "",
   },
 
@@ -726,50 +885,62 @@ export const birdData = [
 
   {
     name: "Budgerigar",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Peach-Faced Lovebird",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Indian Ring-Necked Parakeet",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Yellow Collared Lovebird",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Splendid Grass Parakeet",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Gouldian Finch",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Canary",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Palm Cockatoo",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Cockatiel",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Rooster",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Chick",
+    type: "pet",
     videoId: "",
   },
   {
     name: "Hen",
+    type: "pet",
     videoId: "",
   },
 
@@ -779,30 +950,37 @@ export const birdData = [
 
   {
     name: "Falcon",
+    type: "birdOfPrey",
     videoId: "",
   },
   {
     name: "Kestrel",
+    type: "birdOfPrey",
     videoId: "",
   },
   {
     name: "Condor",
+    type: "birdOfPrey",
     videoId: "",
   },
   {
     name: "Red Kite",
+    type: "birdOfPrey",
     videoId: "",
   },
   {
     name: "Bald Eagle",
+    type: "birdOfPrey",
     videoId: "FZ_chD2mUV8",
   },
   {
     name: "Osprey",
+    type: "birdOfPrey",
     videoId: "",
   },
   {
     name: "Merlin",
+    type: "birdOfPrey",
     videoId: "",
   },
 
@@ -812,27 +990,33 @@ export const birdData = [
 
   {
     name: "Burrowing Owl",
+    type: "night",
     videoId: "7wepVbSeprE",
     vidStart: "27",
   },
   {
     name: "Great Horned Owl",
+    type: "night",
     videoId: "",
   },
   {
     name: "Nighthawk",
+    type: "night",
     videoId: "",
   },
   {
     name: "Spotted Owl",
+    type: "night",
     videoId: "",
   },
   {
     name: "European Nightjar",
+    type: "night",
     videoId: "",
   },
   {
     name: "Owlet-Nightjar",
+    type: "night",
     videoId: "",
   },
 ];
