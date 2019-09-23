b47fe7313ec717b5165528017c9a4d974b19adec
zacaree <zacharem@gmail.com>
2019-09-22 20:39:50 -0500

    Built a nav dropdown

--- Experiments/Gatsby-experiments/learn-gatsbyNode/src/components/header.js
+++ Experiments/Gatsby-experiments/learn-gatsbyNode/src/components/header.js
@@ -1,28 +1,94 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
 import hustlerLogo from "../images/HustlerLogo_OnWhite.svg"
 
-const Header = ({ siteTitle }) => (
-  <header className="header">
-    <div className="wrap ctr__header">
-      <Link to="/">
-        <img className="hustlerLogo" src={hustlerLogo} alt="" />
-      </Link>
-      <nav>
-        <Link to="/products/">Products</Link>
-        <Link to="/blog/">Blog</Link>
-      </nav>
-    </div>
-  </header>
-)
+const Header = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  const data = useStaticQuery(graphql`
+    query {
+      allSanityProducts(sort: { fields: startingPrice, order: ASC }) {
+        edges {
+          node {
+            name
+            valueStatement
+            startingPrice
+            slug {
+              current
+            }
+            heroProductImage {
+              asset {
+                fluid {
+                  ...GatsbySanityImageFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
 
-Header.defaultProps = {
-  siteTitle: ``,
+  console.log(data)
+  return (
+    <header className="header">
+      <div className="wrap ctr__header">
+        <Link to="/">
+          <img className="hustlerLogo" src={hustlerLogo} alt="" />
+        </Link>
+        <nav>
+          <span
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+            className="navItem"
+          >
+            Products
+          </span>
+          <Link to="/blog/">Blog</Link>
+          {dropdownOpen && (
+            <div className="dropdown__products">
+              <div className="dropdown__products--wrap">
+                {data.allSanityProducts.edges.map(({ node }, i) => (
+                  <Link
+                    to={`/products/${node.slug.current}`}
+                    className="product"
+                    key={i}
+                  >
+                    <Image fluid={node.heroProductImage.asset.fluid} />
+                    <h5>{node.name}</h5>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
+
+// export const query = useStaticQuery(graphql`
+//   query {
+//     allSanityProducts(sort: { fields: startingPrice, order: ASC }) {
+//       edges {
+//         node {
+//           name
+//           valueStatement
+//           startingPrice
+//           slug {
+//             current
+//           }
+//           heroProductImage {
+//             asset {
+//               fluid {
+//                 ...GatsbySanityImageFluid
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `)
