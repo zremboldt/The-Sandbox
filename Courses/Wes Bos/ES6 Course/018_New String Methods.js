ac1a4ef3924d28823547771c3c0d7d8f67a99b37
zacaree <zacharem@gmail.com>
2019-10-15 19:53:31 -0500

    Playing with string methods

--- Courses/Wes Bos/ES6 Course/018_New String Methods.js
+++ Courses/Wes Bos/ES6 Course/018_New String Methods.js
@@ -2,23 +2,39 @@
 
 const course = 'RFB2';
 const flightNumber = '20-AC2018-jz';
+const missions = [
+  'Mercury Atlas 6',
+  'Mercury Redstone 3',
+  'Mercury Redstone 4',
+  'Gemini 8',
+  'Gemini 12',
+  'Apollo 11',
+  'Apollo 13'
+];
 const accountNumber = '847563298RT0001';
-
-const make = 'Mercedes';
-const model = 'G5';
-const color = 'Jet Black';
+const vehicle = {
+  make: 'Mercedes',
+  model: 'G5',
+  color: 'Jet Black'
+};
 
 //
 
 // .startsWith()
 console.log(course.startsWith('RFB'));
 console.log(flightNumber.startsWith('AC', 3)); // Start checking on the 3rd index
+// Using .startsWith() to filter
+const startsWithMercury = missions.filter(mission => mission.startsWith('Mercury'));
+console.log(startsWithMercury);
 
 //
 
 // .endsWith()
 console.log(flightNumber.endsWith('jz'));
 console.log(accountNumber.endsWith('RT', 11)); // only check the first 11 numbers
+// Using .endsWith() to filter
+const endsWith13 = missions.filter(mission => mission.endsWith('13'));
+console.log('List includes: ', ...endsWith13);
 
 //
 
@@ -31,11 +47,13 @@ console.log(flightNumber.includes('AC'));
 console.log('🐠 '.repeat(5));
 
 function leftPad(str, length = 20) {
-  return `→ ${' '.repeat(length - str.length)}${str}`;
+  return `→ ${'.'.repeat(length - str.length)} ${str}`;
 }
 
-console.log(leftPad(make));
-console.log(leftPad(model));
-console.log(leftPad(color));
+console.log(leftPad(vehicle.make));
+console.log(leftPad(vehicle.model));
+console.log(leftPad(vehicle.color));
 
 console.log(`${'🦇' * 5}`.repeat(10) + ' Batman!');
+
+missions.forEach(mission => console.log(leftPad(mission, 40)));
