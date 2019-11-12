2acacd7dcbd6b4b721101b502e5f142eb6109370
zacaree <zacharem@gmail.com>
2019-11-11 21:16:54 -0600

    Made some structural changes

--- Experiments/React/react-sandbox/src/index.js
+++ Experiments/React/react-sandbox/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './styles/index.sass';
+import ReactMemo from './components/ReactMemo';
+
+const App = () => (
+  <React.Fragment>
+    <ReactMemo />
+  </React.Fragment>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
