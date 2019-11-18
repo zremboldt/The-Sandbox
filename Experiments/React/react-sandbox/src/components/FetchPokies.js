77091d686623947864618756fe0ab2ec3249b4fd
zacaree <zacharem@gmail.com>
2019-11-17 22:27:05 -0600

    Playing with mock data in the airport

--- Experiments/React/react-sandbox/src/components/FetchPokies.js
+++ Experiments/React/react-sandbox/src/components/FetchPokies.js
@@ -1,27 +1,56 @@
 import React, { useState, useEffect } from 'react';
 
+const mockData = [
+  { id: 0, name: 'Bulbasaur' },
+  { id: 1, name: 'Squirtle' },
+  { id: 2, name: 'Charmander' },
+  { id: 3, name: 'Pikachu' },
+  { id: 4, name: 'Dragonite' }
+];
+
 const FetchPokies = () => {
+  const [currentId, setCurrentId] = useState(0);
   const [data, setData] = useState();
 
+  console.log(data);
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/')
-      .then(res => res.json())
-      .then(json => setData(json.results));
-  }, []);
+    console.log(currentId);
+    // Mock fetch
+    setData(mockData[currentId]);
 
-  if (data !== undefined) {
-    return (
-      <ul>
-        {data.map((item, i) => (
-          <li key={i}>
-            <a href={item.url}>{item.name}</a>
-          </li>
-        ))}
-      </ul>
-    );
-  } else {
-    return 'Loading...';
-  }
+    // fetch('https://pokeapi.co/api/v2/pokemon/')
+    //   .then(res => res.json())
+    //   .then(json => setData(json.results));
+  }, [currentId]);
+
+  // if (data !== undefined) {
+  return (
+    <React.Fragment>
+      <h3>{currentId}</h3>
+      {/* <h3>{data}</h3> */}
+      <div className="ctr-buttons">
+        <button onClick={() => setCurrentId(currentId - 1)}>←</button>
+        <button onClick={() => setCurrentId(currentId + 1)}>→</button>
+      </div>
+    </React.Fragment>
+  );
+  // } else {
+  //   return 'Loading...';
+  // }
+
+  // if (data !== undefined) {
+  //   return (
+  //     <ul>
+  //       {data.map((item, i) => (
+  //         <li key={i}>
+  //           <a href={item.url}>{item.name}</a>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+  // } else {
+  //   return 'Loading...';
+  // }
 };
 
 export default FetchPokies;
--- Experiments/React/react-sandbox/src/index.js
+++ Experiments/React/react-sandbox/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.sass';
 // import ReactMemo from './components/ReactMemo';
-// import FetchPokies from './components/FetchPokies';
-import CompositionAvoidsPropDrilling from './components/CompositionAvoidsPropDrilling';
+import FetchPokies from './components/FetchPokies';
+// import CompositionAvoidsPropDrilling from './components/CompositionAvoidsPropDrilling';
 
 const App = () => (
   <React.Fragment>
     {/* <ReactMemo /> */}
-    {/* <FetchPokies /> */}
-    <CompositionAvoidsPropDrilling />
+    <FetchPokies />
+    {/* <CompositionAvoidsPropDrilling /> */}
   </React.Fragment>
 );
 
