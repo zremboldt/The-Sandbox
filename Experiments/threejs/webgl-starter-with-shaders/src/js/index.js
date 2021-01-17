242b47ba9b73f81e484d3fe87eca24f35ae3b58a
Zac Remboldt <zac.remboldt@joinroot.com>
2021-01-16 21:01:18 -0600

    Update starter

--- Experiments/threejs/webgl-starter-with-shaders/src/js/index.js
+++ Experiments/threejs/webgl-starter-with-shaders/src/js/index.js
@@ -1,24 +1,25 @@
 import * as THREE from "three";
-import fragment from './shaders/fragment.glsl';
-import vertex from './shaders/vertex.glsl';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GUI } from 'three/examples/jsm/libs/dat.gui.module';
+import fragment from './shaders/fragment.glsl';
+import vertex from './shaders/vertex.glsl';
 import imgAbove from '../assets/above.jpg';
 import displacement from '../assets/displacement.png';
 
 export default class Sketch{
   constructor() {
+    this.scene = new THREE.Scene();
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.setClearColor(0xeeeeee, 1);
     document.getElementById('container').appendChild( this.renderer.domElement );
 
     this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    this.camera.position.z = 1;
-    this.scene = new THREE.Scene();
+    this.camera.position.set(0,0,1);
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.settings();
     this.time = 0;
     this.addObjects();
+    this.settings();
     this.render();
   }
 
@@ -31,8 +32,6 @@ export default class Sketch{
   }
 
   addObjects() {
-    // console.log(this.time)
-    // console.log(time)
     this.material = new THREE.ShaderMaterial({
       extensions: { derivatives: '#extension GL_OES_standard_derivatives : enable' },
       side: THREE.DoubleSide,
