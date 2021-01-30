f4421c7af14164fa53856bbbb50980f8f6c6c614
Zac Remboldt <zac.remboldt@joinroot.com>
2021-01-29 19:50:56 -0600

    .

--- Experiments/threejs/shader-w-scroll-triger-w-css-grid/src/js/shaders/fragment.glsl
+++ Experiments/threejs/shader-w-scroll-triger-w-css-grid/src/js/shaders/fragment.glsl
@@ -3,19 +3,15 @@ varying vec2 vUv;
 void main() {
   // COORDINATE LAYOUT
   vec2 uv = vUv * 2.0 - 1.0;
-  // This next line increases the scale of the workspace.
-  // Now coordinates run from 0 to 2 instead of 0 to 1.
-  uv *= 2.;
-  // This next line will shift the workspace around.
-  uv += 1.0;
-
-  // `distance` asks, how far is the current pixel `uv`, from the origin point?
-  float dist = distance(uv, vec2(0, 0));
+  
+  vec2 gradient = abs(uv);
+  float squareGradient = max(gradient.x, gradient.y);
+  float square = step(0.5, squareGradient);
 
   // COLORS
   vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
 
-  color.r = step(0.5, dist);
+  color.r = 1.0 - square;
   color.g = 0.0;
   color.b = 0.0;
 
