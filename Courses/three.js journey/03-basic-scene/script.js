ce3c8dfcbe1205f0c60e8067c5304ab73948fd00
Zac Remboldt <zac.remboldt@joinroot.com>
2021-03-25 21:59:35 -0400

    Lesson 3 - three.js jounrey

---
+++ Courses/three.js journey/03-basic-scene/script.js
@@ -0,0 +1,23 @@
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const fieldOfView = 75;
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio);
+camera.position.z = 3;
+scene.add(camera);
+
+const canvasEl = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas: canvasEl });
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
