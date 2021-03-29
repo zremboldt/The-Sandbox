9fd252a2df591ac8cbbb257128ae82710513404a
Zac Remboldt <zac.remboldt@joinroot.com>
2021-03-28 20:17:03 -0400

    .

--- Courses/three.js journey/06-animations/src/script.js
+++ Courses/three.js journey/06-animations/src/script.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene()
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
