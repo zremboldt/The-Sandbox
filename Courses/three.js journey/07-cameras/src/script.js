735cd89fb5f2412b6cb029795afa195fce6dd149
Zac Remboldt <zac.remboldt@joinroot.com>
2021-03-28 22:08:16 -0400

    Working through lesson 7

---
+++ Courses/three.js journey/07-cameras/src/script.js
@@ -0,0 +1,66 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Cursor
+const cursor = { x: 0, y: 0 };
+window.addEventListener('mousemove', (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = - (e.clientY / sizes.height - 0.5);
+  console.log(cursor)
+})
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600
+}
+const aspectRatio = sizes.width / sizes.height;
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, aspectRatio)
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
+camera.lookAt(mesh.position)
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update objects
+  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  camera.position.y = cursor.y * 5;
+
+  camera.lookAt(mesh.position)
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
