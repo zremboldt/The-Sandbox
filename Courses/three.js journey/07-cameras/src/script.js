38153f271ed231e1bd96180ec798553702d11dff
Zac Remboldt <zac.remboldt@joinroot.com>
2021-03-29 22:06:58 -0400

    Finished lesson 7

--- Courses/three.js journey/07-cameras/src/script.js
+++ Courses/three.js journey/07-cameras/src/script.js
@@ -1,12 +1,12 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 // Cursor
 const cursor = { x: 0, y: 0 };
 window.addEventListener('mousemove', (e) => {
   cursor.x = e.clientX / sizes.width - 0.5;
   cursor.y = - (e.clientY / sizes.height - 0.5);
-  console.log(cursor)
 })
 
 // Canvas
@@ -25,22 +25,22 @@ const scene = new THREE.Scene()
 // Object
 const mesh = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+  new THREE.MeshNormalMaterial()
 )
 scene.add(mesh)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, aspectRatio)
-// camera.position.x = 2
-// camera.position.y = 2
 camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
 // Renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
 renderer.setSize(sizes.width, sizes.height)
 
 // Animate
@@ -50,11 +50,15 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime()
 
   // Update objects
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 5;
-
-  camera.lookAt(mesh.position)
+  const pulse = 1 + Math.sin(elapsedTime * 5) *  0.1;
+  mesh.scale.set(pulse,pulse,pulse);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position)
+  
+  // Update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera)
