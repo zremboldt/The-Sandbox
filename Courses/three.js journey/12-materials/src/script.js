73fd99d23da307b4e551c18c3436894e2b3f68e0
Zac Remboldt <zac.remboldt@joinroot.com>
2021-04-10 07:43:59 -0400

    Working through lesson on materials

---
+++ Courses/three.js journey/12-materials/src/script.js
@@ -0,0 +1,144 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+// Textures
+const textureLoader = new THREE.TextureLoader();
+
+const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg');
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg');
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg');
+const doorHeightTexture = textureLoader.load('./textures/door/height.jpg');
+const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg');
+const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg');
+const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg');
+const gradient3Texture = textureLoader.load('./textures/gradients/3.jpg');
+const matcapTexture = textureLoader.load('./textures/matcaps/8.png');
+const matcapRoot1Texture = textureLoader.load('./textures/matcaps/root-matcap-1.png');
+const matcapRoot2Texture = textureLoader.load('./textures/matcaps/root-matcap-2.png');
+const matcapRootOffWhiteTexture = textureLoader.load('./textures/matcaps/off-white-matcap-1.png');
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// ===========================
+// Objects
+// ===========================
+
+// const material = new THREE.MeshBasicMaterial();
+// material.color = new THREE.Color(0x00ff00);
+// material.wireframe = true;
+// material.transparent = true;
+// material.opacity = 0.5;
+// material.map = doorColorTexture;
+// material.alphaMap = doorAlphaTexture;
+// material.side = THREE.DoubleSide; // Avoid unless needed because it requires more calculations for the CPU
+
+// const material = new THREE.MeshNormalMaterial();
+// material.wireframe = true;
+// material.side = THREE.DoubleSide;
+// material.flatShading = true;
+
+const material = new THREE.MeshMatcapMaterial();
+material.matcap = matcapRoot1Texture;
+// material.matcap = matcapTexture;
+// material.flatShading = true;
+material.side = THREE.DoubleSide;
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 160, 160),
+  material
+)
+sphere.position.x = -1.5
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 1, 8, 8),
+  material
+)
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 160, 320),
+  material
+)
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus);
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime;
+    plane.rotation.y = 0.1 * elapsedTime;
+    torus.rotation.y = 0.1 * elapsedTime;
+
+    sphere.rotation.x = 0.15 * elapsedTime;
+    plane.rotation.x = 0.15 * elapsedTime;
+    torus.rotation.x = 0.15 * elapsedTime;
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
