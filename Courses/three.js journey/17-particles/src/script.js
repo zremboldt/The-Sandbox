d2729cbc443bdc491f98d5b8fd42946235deec86
Zac Remboldt <zac.remboldt@joinroot.com>
2021-04-20 22:42:16 -0400

    Working through lesson on particles

--- Courses/three.js journey/17-particles/src/script.js
+++ Courses/three.js journey/17-particles/src/script.js
@@ -2,6 +2,12 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { AdditiveBlending } from 'three'
+
+// ====================================
+// Nice freee assets here
+// https://www.kenney.nl/assets/
+// ====================================
 
 /**
  * Base
@@ -19,6 +25,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('./textures/particles/9.png');
 
 // =========================================
 // Particles
@@ -35,24 +42,82 @@ const textureLoader = new THREE.TextureLoader()
 
 // - - - - - - - - - - - - - - - - - - - - -
 
-const geometry = new THREE.BufferGeometry();
+// const geometry = new THREE.BufferGeometry();
 
-const count = 10000;
-const positionsArray = new Float32Array(count * 3 * 3);
+// const count = 10000;
+// const positionsArray = new Float32Array(count * 3 * 3);
 
-for (let i = 0; i < count * 3 * 3; i++) {
-  positionsArray[i] = Math.random() - 0.5;
-}
+// for (let i = 0; i < count * 3 * 3; i++) {
+//   positionsArray[i] = Math.random() - 0.5;
+// }
 
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
-geometry.setAttribute('position', positionsAttribute);
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+// geometry.setAttribute('position', positionsAttribute);
 
-const material = new THREE.PointsMaterial({ size: 0.001, sizeAttenuation: true })
-const particles = new THREE.Points(geometry, material)
-scene.add(particles);
+// const material = new THREE.PointsMaterial({ size: 0.001, sizeAttenuation: true })
+// const particles = new THREE.Points(geometry, material)
+// scene.add(particles);
 
 // - - - - - - - - - - - - - - - - - - - - -
 
+// const geometry = new THREE.BufferGeometry();
+
+// const vertexCount = 20000;
+
+// const positions = new Float32Array(vertexCount * 3);
+
+// for (let i = 0; i < vertexCount * 3; i++) {
+//   positions[i] = (Math.random() - 0.5) * 10;
+// }
+
+// geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Why 3 here? because we need to say that every 3 values in the array make up 1 vertex.
+
+// const material = new THREE.PointsMaterial({ 
+//   transparent: true,
+//   alphaMap: particleTexture,
+//   // alphaTest: 0.001, // Method 1.
+//   // depthTest: false, // Method 2.
+//   depthWrite: false, // Method 3. This is often the best way to make the the black portion of your particle image transparent.
+//   blending: AdditiveBlending, // Warning, may impact perf. Good for making things appear to glow.
+//   color: 'hsl(20, 100%, 50%)',
+//   size: 0.1, 
+//   sizeAttenuation: true
+// });
+// const particles = new THREE.Points(geometry, material);
+// scene.add(particles);
+
+// - - - - - - - - - - - - - - - - - - - - -
+
+const geometry = new THREE.BufferGeometry();
+
+const vertexCount = 20000;
+
+const positions = new Float32Array(vertexCount * 3); // 3 here refers to the three points in the triangular vertex.
+// Add a color attribute per particle
+const colors = new Float32Array(vertexCount * 3); // 3 here refers to "R", "G", "B".
+
+for (let i = 0; i < vertexCount * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
+}
+
+geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Why 3 here? because we need to say that every 3 values in the array make up 1 vertex.
+geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3)) // Why 3 here? because we need to say that every 3 values in the array make up 1 vertex.
+
+const material = new THREE.PointsMaterial({ 
+  transparent: true,
+  alphaMap: particleTexture,
+  // alphaTest: 0.001, // Method 1.
+  // depthTest: false, // Method 2.
+  depthWrite: false, // Method 3. This is often the best way to make the the black portion of your particle image transparent.
+  blending: AdditiveBlending,
+  // color: 'hsl(20, 100%, 50%)',
+  vertexColors: true,
+  size: 0.1, 
+  sizeAttenuation: true
+});
+const particles = new THREE.Points(geometry, material);
+scene.add(particles);
 /**
  * Sizes
  */
