721f1f7567db4850ef74be4393f36db49d93de8e
Zac Remboldt <zac.remboldt@joinroot.com>
2021-06-25 22:49:41 -0500

    perlin-noise-applied-to-vertex-&-fragment-shaders

---
+++ Courses/Yuri Artiukh/Merging WebGL and HTML worlds/003/js/shaders/fragment.glsl
@@ -0,0 +1,9 @@
+varying float vNoise;
+
+void main() {
+  vec3 color1 = vec3(1.,0.,0.);
+  vec3 color2 = vec3(0.,0.,1.);
+  vec3 finalColor = mix(color1,color2,0.5 * (vNoise + 1.));
+
+  gl_FragColor = vec4(finalColor, 1.);
+}
