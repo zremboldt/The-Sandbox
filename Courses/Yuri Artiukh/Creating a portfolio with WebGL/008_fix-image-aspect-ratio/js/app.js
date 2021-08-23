05c4b34c4832b100d20660913b358adce772c5d6
Zac Remboldt <zac.remboldt@joinroot.com>
2021-08-22 20:53:27 -0500

    Finished handling UV img aspect ratio

---
+++ Courses/Yuri Artiukh/Creating a portfolio with WebGL/008_fix-image-aspect-ratio/js/app.js
@@ -0,0 +1,99 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import debounce from 'lodash.debounce';
+import vertex from '../shaders/vertex.glsl';
+import fragment from '../shaders/fragment.glsl';
+import testTexture from '../images/texture.jpg';
+import calculateCameraFov from './calculate-camera-fov'
+import * as dat from 'dat.gui';
+
+
+
+let camera, scene, renderer;
+let geometry, material, mesh;
+let containerEl;
+
+init();
+
+function init() {
+  containerEl = document.getElementById('container_webgl');
+  const { offsetWidth, offsetHeight } = containerEl;
+
+  // Camera & scene
+  const cameraZPosition = 600;
+  camera = new THREE.PerspectiveCamera(calculateCameraFov(cameraZPosition, offsetHeight), offsetWidth / offsetHeight, 10, 1000);
+  camera.position.z = cameraZPosition;
+  scene = new THREE.Scene();
+
+  // 3D objects
+  addObjects();
+  setupSettings();
+
+  // Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(offsetWidth, offsetHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setAnimationLoop(animation);
+  containerEl.appendChild(renderer.domElement);
+
+  // Controls
+  controls = new OrbitControls(camera, renderer.domElement)
+
+  // Resize listener
+  window.addEventListener('resize', debounce(resizeWindow, 300))
+}
+
+//
+
+function animation(time) {
+  // mesh.rotation.x = time / 2000;
+  // mesh.rotation.y = time / 1000;
+
+  const seconds = (time * 0.001);
+
+  material.uniforms.uTime.value = seconds;
+  material.uniforms.uProgress.value = settings.progress;
+
+  renderer.render(scene, camera);
+}
+
+//
+
+function addObjects() {
+  const { offsetWidth, offsetHeight } = containerEl;
+
+  geometry = new THREE.PlaneBufferGeometry(350, 350, 1, 1);
+  material = new THREE.ShaderMaterial({
+    side: THREE.DoubleSide,
+    uniforms: {
+      uTime: { value: 1.0 },
+      uProgress: { value: 0.0 },
+      uResolution: { value: new THREE.Vector2(offsetWidth, offsetHeight) },
+      uQuadSize: { value: new THREE.Vector2(350, 350) },
+      uTexture: { value: new THREE.TextureLoader().load(testTexture) },
+      uTextureSize: { value: new THREE.Vector2(100, 100) }
+    },
+    vertexShader: vertex,
+    fragmentShader: fragment
+  });
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = 300;
+  mesh.rotation.z = 0.5;
+  scene.add(mesh);
+}
+
+function resizeWindow() {
+  const { offsetWidth, offsetHeight } = containerEl;
+
+  renderer.setSize(offsetWidth, offsetHeight);
+  camera.aspect = offsetWidth / offsetHeight;
+  camera.updateProjectionMatrix();
+}
+
+function setupSettings() {
+  settings = {
+    progress: 0
+  }
+  gui = new dat.GUI();
+  gui.add(settings, 'progress', 0, 1, 0.001)
+}
---
+++ Courses/Yuri Artiukh/Creating a portfolio with WebGL/008_fix-image-aspect-ratio/shaders/vertex.glsl
@@ -0,0 +1,22 @@
+uniform float uTime;
+uniform float uProgress;
+uniform vec2 uResolution;
+uniform vec2 uQuadSize;
+varying vec2 vUv;
+varying vec2 vSize;
+
+void main() {
+  vUv = uv;
+  vec3 pos = position;
+
+  vec4 defaultState = modelMatrix * vec4(pos, 1.0);
+  vec4 fullScreenState = vec4(pos, 1.0);
+  fullScreenState.x *= uResolution.x / uQuadSize.x;
+  fullScreenState.y *= uResolution.y / uQuadSize.y;
+
+  vec4 finalState = mix(defaultState, fullScreenState, uProgress);
+
+  vSize = mix(uQuadSize, uResolution, uProgress);
+
+  gl_Position = projectionMatrix * viewMatrix * finalState;
+}
