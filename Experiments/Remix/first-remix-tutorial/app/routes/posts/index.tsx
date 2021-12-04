8b49fe50a9d93dde6c8df91e3a28a444e6e80643
Zac Remboldt <zac.remboldt@joinroot.com>
2021-12-03 20:55:15 -0600

    Trying out Remix

---
+++ Experiments/Remix/first-remix-tutorial/app/routes/posts/index.tsx
@@ -0,0 +1,24 @@
+import { Link, useLoaderData } from 'remix';
+import { getPosts } from '~/post';
+import type { Post } from '~/post';
+
+export const loader = () => {
+  return getPosts();
+}
+
+export default function Posts() {
+  const posts = useLoaderData<Post[]>();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(({slug, title}) => (
+          <li key={slug}>
+            <Link to={slug}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
