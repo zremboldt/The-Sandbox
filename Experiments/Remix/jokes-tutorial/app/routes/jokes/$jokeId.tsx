f4f02ef46288b0ed743b6b40c6d48da07c4bbee4
Zac Remboldt <zac.remboldt@joinroot.com>
2021-12-13 22:22:49 -0600

    .

--- Experiments/Remix/jokes-tutorial/app/routes/jokes/$jokeId.tsx
+++ Experiments/Remix/jokes-tutorial/app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,29 @@
+import { Joke } from "@prisma/client";
+import type { LoaderFunction } from "remix";
+import { useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: Joke | null }
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const data: LoaderData = {
+    joke: await db.joke.findUnique({
+      select: { content: true },
+      where: { id: params.jokeId }
+    })
+  }
+  return data;
+}
+
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
+
+  console.log(data)
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees?
-        They're really good at it.
-      </p>
+      <p>{data.joke?.content}</p>
     </div>
   );
 }
