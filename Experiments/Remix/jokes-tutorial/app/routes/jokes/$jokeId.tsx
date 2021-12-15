351b013d0ece6049e895b7aed326c1feb49bedef
Zac Remboldt <zac.remboldt@joinroot.com>
2021-12-14 20:31:39 -0600

    Finish generating page based on id

--- Experiments/Remix/jokes-tutorial/app/routes/jokes/$jokeId.tsx
+++ Experiments/Remix/jokes-tutorial/app/routes/jokes/$jokeId.tsx
@@ -1,29 +1,28 @@
-import { Joke } from "@prisma/client";
 import type { LoaderFunction } from "remix";
-import { useLoaderData } from "remix";
+import { Link, useLoaderData } from "remix";
+import { Joke } from "@prisma/client";
 import { db } from "~/utils/db.server";
 
-type LoaderData = { joke: Joke | null }
+type LoaderData = { joke: Joke }
 
 export const loader: LoaderFunction = async ({ params }) => {
-  const data: LoaderData = {
-    joke: await db.joke.findUnique({
-      select: { content: true },
-      where: { id: params.jokeId }
-    })
-  }
+  const joke = await db.joke.findUnique({
+    // select: { content: true },
+    where: { id: params.jokeId }
+  })
+  if (!joke) throw new Error("Joke not found");
+  const data: LoaderData = { joke };
   return data;
 }
 
 export default function JokeRoute() {
   const data = useLoaderData<LoaderData>();
 
-  console.log(data)
-
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>{data.joke?.content}</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} permalink</Link>
     </div>
   );
 }
