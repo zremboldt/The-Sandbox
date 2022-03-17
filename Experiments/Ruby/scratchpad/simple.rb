5856798d27d51ae185087247d5055ebd3e8b89b8
zremboldt <zac.remboldt@joinroot.com>
2022-03-17 08:11:22 -0500

    .

--- Experiments/Ruby/scratchpad/simple.rb
+++ Experiments/Ruby/scratchpad/simple.rb
@@ -1,2 +1,25 @@
-puts 123
-puts 456
+class Human
+  def initialize(first_name, middle_name, last_name, age)
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
+    @age = age
+  end
+
+  def simple_print
+    puts "#{@first_name} #{@middle_name} #{@last_name}"
+  end
+
+  def complex_print
+    puts "First name: #{@first_name}"
+    puts "Middle name: #{@middle_name}"
+    puts "Last name: #{@last_name}"
+    puts "Age: #{@age}"
+  end
+end
+
+human1 = Human.new("John", "Paul", "Jones", "30")
+human2 = Human.new("Danny", "Paul", "King", "30")
+
+puts human1.simple_print
+puts human2.complex_print
