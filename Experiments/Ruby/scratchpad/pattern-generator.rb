78c351dfaa49a73d82d46b62bc7fe4e1e9317e47
zremboldt <zac.remboldt@joinroot.com>
2022-04-24 13:49:33 -0500

    Refining pattern generator

--- Experiments/Ruby/scratchpad/pattern-generator.rb
+++ Experiments/Ruby/scratchpad/pattern-generator.rb
@@ -1,55 +1,32 @@
 row_count = 10
-column_count = row_count * 2.5
+column_count = row_count * 2.4
 
-patterns = {
-  # p0: ['⋅', '=', '='],
-  p1: ['╲', '╲', '╳'],
-  # p2: ['┆', '┆', '⋅'],
-  # p3: ['┆', '⋅', ''],
-  p4: ['⋅', '╲', '╱'],
-  # p5: ['╮', '┼', '╰'],
-  # p6: ['┼', '┼', ' '],
-  # p7: ['⏐', '┆', '⏐'],
-  # p8: ['✦', '✧', ''],
-  # p9: ['✕', '+', ' '],
-  # p10: ['≠', '≎', '≑'],
-  # p11: ['∶', '', '∴'],
-  # x: ['╳', '╳', '╳'],
-  diagonal_maze: ['╲', '╲', '╱'],
-  # diagonals: ['╲', '╲', '╲'],
-  emojis: ['🪲 ', '🐞 ', ' '],
-}
+patterns = [
+  ['┼', '┼', ' '],
+  ['✦', '✧', ' '],
+  ['✕', '+', ' '],
+  ['∶', '∷', ' '],
+  ['⋅', '=', '='],
+  ['┆', '┆', '⋅'],
+  ['╮', '┼', '╰'],
+  ['⏐', '┆', '⏐'],
+  ['≠', '≎', '≑'],
+  ['⋅', '╲', '╱'],
+  ['╲', '╱', '╳'],
+  ['╲', '╲', '╱'],
+  ['╳', '╳', '╳'],
+  ['╲', '╲', '╲'],
+]
 
-randomPattern = patterns.values[rand(patterns.values.length)]
+randomPattern = patterns[rand(patterns.length())]
 
-selectedPattern = randomPattern
-# selectedPattern = patterns[:p11]
-
-i = 0
-loop do
+row_count.times do
   arr = []
   
-  loop do
-    random_num = rand(1..4)
-    
-    case random_num
-    when 1
-      arr.push(selectedPattern[0])
-    when 2
-      arr.push(selectedPattern[1])
-    when 3
-      arr.push(selectedPattern[2])
-    end
-
-    if arr.length == column_count
-      break
-    end
+  while arr.length < column_count
+    random_num = rand(0..2)
+    arr.push(randomPattern[random_num])
   end
   
   puts arr.join('')
-  
-  i += 1
-  if i == row_count
-    break
-  end
 end
