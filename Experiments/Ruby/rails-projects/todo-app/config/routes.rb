29195739c458a26d3f42bf237e938a24b2449316
zremboldt <zac.remboldt@joinroot.com>
2022-07-08 13:05:52 -0500

    Ran `rails g nested_scaffold`

--- Experiments/Ruby/rails-projects/todo-app/config/routes.rb
+++ Experiments/Ruby/rails-projects/todo-app/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items
+  end
+  namespace :todo_list do
+    resources :todo_items
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
