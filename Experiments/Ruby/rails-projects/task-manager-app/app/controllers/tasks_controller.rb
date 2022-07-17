844e8bc5e3fae0d00648be989e1c3aab60b7ca17
zremboldt <zac.remboldt@joinroot.com>
2022-07-16 22:11:19 -0500

    More rails learning

--- Experiments/Ruby/rails-projects/task-manager-app/app/controllers/tasks_controller.rb
+++ Experiments/Ruby/rails-projects/task-manager-app/app/controllers/tasks_controller.rb
@@ -2,10 +2,25 @@ class TasksController < ApplicationController
   def index
     @todos = ["Paint the baseboards", "Mow the lawn", "Fix the basement floor drain", "Skimcoat the living room wall", "Clean out the dishwasher", "Keep learning Rails"]
   end
+  
+  def show
+  end
 
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
   end
 end
--- Experiments/Ruby/rails-projects/task-manager-app/config/routes.rb
+++ Experiments/Ruby/rails-projects/task-manager-app/config/routes.rb
@@ -1,12 +1,33 @@
 Rails.application.routes.draw do
   match "/", to: "main#index", via: :get
 
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/edit'
-
   match "about", to: "main#about", via: :get
   match "hello", to: "main#hello", via: :get
+
+  resources :tasks do
+    member do
+      get :delete
+    end
+  end
+  
+  resources :categories do
+    member do
+      get :delete
+    end
+  end
+
+
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
+  # get 'categories/edit'
+  # get 'categories/delete'
+
+  # get 'tasks/index'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+  # get 'tasks/show'
+  # get 'tasks/delete'
   
   # get 'main/index'
   # match "main", to: "main#index", via: :get
