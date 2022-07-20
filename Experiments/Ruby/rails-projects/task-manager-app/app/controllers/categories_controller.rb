4822f1404510227e2b3b880fd93d8e4a062c1fb2
zremboldt <zac.remboldt@joinroot.com>
2022-07-20 18:37:31 -0500

    More work on Rails 7 Essential Training

--- Experiments/Ruby/rails-projects/task-manager-app/app/controllers/categories_controller.rb
+++ Experiments/Ruby/rails-projects/task-manager-app/app/controllers/categories_controller.rb
@@ -3,12 +3,24 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
+    @tasks = Task.where(category_id: @category)
+    @completed_tasks = completed_tasks
+    @total_tasks = total_tasks
+    @percent_complete = percent_complete
   end
 
   def new
+    @category = Category.new
   end
-
+  
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to "/"
+    else
+      render("new")
+    end
   end
 
   def edit
@@ -18,8 +30,42 @@ class CategoriesController < ApplicationController
   end
 
   def delete
+    @category = Category.find(params[:id])
   end
 
   def destroy
-  def
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to "/"
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def percent_complete
+    return 0 if total_tasks == 0
+    (100 * completed_tasks.to_f / total_tasks).round(1)
+  end
+
+  def completed_tasks
+    @completed_tasks ||= @tasks.where(completed: true).count
+  end
+
+  def total_tasks
+    @total_tasks ||= @tasks.count
+  end
+
+  def status
+    case percent_complete.to_i
+    when 0
+      'Not started'
+    when 100
+      'Complete'
+    else
+      'In progress'
+    end
+  end
 end
--- Experiments/Ruby/rails-projects/task-manager-app/app/controllers/tasks_controller.rb
+++ Experiments/Ruby/rails-projects/task-manager-app/app/controllers/tasks_controller.rb
@@ -1,26 +1,63 @@
 class TasksController < ApplicationController
   def index
-    @todos = ["Paint the baseboards", "Mow the lawn", "Fix the basement floor drain", "Skimcoat the living room wall", "Clean out the dishwasher", "Keep learning Rails"]
+    @page = params[:page].to_i
+    @tasks = Task.order(:position)
   end
   
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @count = Task.count
+    @task = Task.new(position: @count + 1)
+    @categories = Category.all
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      # The new action is not being called here this is just rendering the new template
+      render('new')
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
+    @categories = Category.all
   end
-
+  
   def update
+    # @category = Category.find(params[:category_id])
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @task = Task.find(params[:id])
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
-  def destroy
+  private
+
+  def task_params
+    params.require(:task).permit(
+      :name, 
+      :position, 
+      :category_id,
+      :completed, 
+      :description
+    )
   end
 end
