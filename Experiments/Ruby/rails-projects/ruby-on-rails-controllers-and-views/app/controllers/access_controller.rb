f8037e390d6cacc1dd3264d1095ae493522523bc
zremboldt <zac.remboldt@joinroot.com>
2022-07-21 07:40:21 -0500

    Initial commit for Controllers and Views course

---
+++ Experiments/Ruby/rails-projects/ruby-on-rails-controllers-and-views/app/controllers/access_controller.rb
@@ -0,0 +1,25 @@
+class AccessController < ApplicationController
+
+  # display menu
+  def menu
+    @username = cookies[:username]
+  end
+
+  # display login form
+  def new
+  end
+
+  # processs login form
+  def create
+    # do login process here
+    cookies[:username] = params[:username]
+    redirect_to(menu_path)
+  end
+
+  # logout user
+  def destroy
+    # do logout process here
+    redirect_to(login_path)
+  end
+
+end
