159610503bfb900c635706b8b31731b7e3d27867
zremboldt <zac.remboldt@joinroot.com>
2022-11-08 16:30:51 -0600

    Fix rerender on menu open

--- Experiments/React/verses/components/nav.js
+++ Experiments/React/verses/components/nav.js
@@ -1,20 +1,30 @@
 import { Cross as MenuIcon } from "hamburger-react";
 import Image from "next/image";
 import Logo from "../assets/logo.png";
+import { useState } from "react";
+import Menu from "./menu";
 
-export default function Nav({ isMenuVisible, setIsMenuVisible }) {
-  // return null
+export default function Nav({ setSelectedVerse, data }) {
+  const [isMenuVisible, setIsMenuVisible] = useState(true);
+
   return (
-    <nav>
-      <Image src={Logo} alt="" width={140} />
-      {/* <h2 className={"logo"}>✨ Word</h2> */}
-      {/* <img src={Logo} alt="" /> */}
+    <>
+      <nav>
+        <Image src={Logo} alt="" width={140} />
 
-      <MenuIcon
-        toggled={isMenuVisible}
-        toggle={() => setIsMenuVisible(!isMenuVisible)}
-        size={28}
+        <MenuIcon
+          toggled={isMenuVisible}
+          toggle={() => setIsMenuVisible(!isMenuVisible)}
+          size={28}
+        />
+      </nav>
+
+      <Menu
+        setSelectedVerse={setSelectedVerse}
+        data={data}
+        setIsMenuVisible={setIsMenuVisible}
+        isMenuVisible={isMenuVisible}
       />
-    </nav>
+    </>
   );
 }
--- Experiments/React/verses/pages/index.js
+++ Experiments/React/verses/pages/index.js
@@ -3,15 +3,11 @@ import { getDatabase, getBlocks } from "../lib/notion";
 import Nav from "../components/nav";
 import { useState } from "react";
 import VerseView from "../components/verse-view";
-import Menu from "../components/menu";
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
 export default function Home({ data }) {
-  const [isMenuVisible, setIsMenuVisible] = useState(true);
   const [selectedVerse, setSelectedVerse] = useState(null);
-
-  const day = dayOfTheWeek();
 
   return (
     <div>
@@ -20,14 +16,7 @@ export default function Home({ data }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Nav isMenuVisible={isMenuVisible} setIsMenuVisible={setIsMenuVisible} />
-
-      <Menu
-        setSelectedVerse={setSelectedVerse}
-        data={data}
-        setIsMenuVisible={setIsMenuVisible}
-        isMenuVisible={isMenuVisible}
-      />
+      <Nav setSelectedVerse={setSelectedVerse} data={data} />
 
       <VerseView blocks={selectedVerse} />
     </div>
@@ -55,10 +44,4 @@ export const getStaticProps = async () => {
     props: { data: transformedData },
     revalidate: 1,
   };
-};
-
-const dayOfTheWeek = () => {
-  const options = { weekday: "long" };
-  const now = new Date();
-  return new Intl.DateTimeFormat("en-US", options).format(now);
 };
