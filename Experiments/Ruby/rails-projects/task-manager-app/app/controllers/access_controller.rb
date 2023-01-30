cd9e4dde3ae432506a044df7e67a38b74bb260e7
zremboldt <zac.remboldt@joinroot.com>
2023-01-30 09:06:46 -0600

    learning

--- Experiments/Ruby/rails-projects/task-manager-app/app/controllers/access_controller.rb
+++ Experiments/Ruby/rails-projects/task-manager-app/app/controllers/access_controller.rb
@@ -2,26 +2,37 @@ class AccessController < ApplicationController
 
   # display menu
   def menu
-    @username = cookies[:username]
-    @user_id = session[:user_id]
+    get_user_info_from_session
   end
 
   # display login form
   def new
+    get_user_info_from_session
+
+    if logged_in?
+      redirect_to(menu_path)
+    end
   end
 
   # processs login form
   def create
+    logger.info("***** Login user #{params[:username]}")
+
     # do login process here
     cookies[:username] = params[:username]
     session[:user_id] = 1483
+    flash[:notice] = "Login successful!"
     redirect_to(menu_path)
   end
 
   # logout user
   def destroy
+    logger.info("***** Logout user #{cookies[:username]}")
+
     # do logout process here
+    cookies[:username] = nil
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
     redirect_to(login_path)
   end
-
 end
