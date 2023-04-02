3585592073bebeac4e508c636440325ad96feae7
zremboldt <zac.remboldt@joinroot.com>
2023-04-01 21:41:59 -0500

    wave-function-collapse

---
+++ Experiments/wave-function-collapse/roads/src/App.js
@@ -0,0 +1,13 @@
+import Gameboard from './gameboard'
+
+function App() {
+  return (
+    <div className="App">
+      <Gameboard />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ Experiments/wave-function-collapse/roads/src/gameboard.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { tileDefinition } from './tile-definition';
+
+export default function Gameboard({ boardWidth = 10, gridCellSize = 80 }) {
+  const [mapLayout, setMapLayout] = useState();
+  const boardWidthInPx = gridCellSize * (boardWidth - 1);
+
+  useEffect(() => {
+    const boardHeight = boardWidth;
+    let tileDistribution = [];
+
+    for (let i = 0; i < boardHeight; i++) {
+      for (let j = 0; j < boardWidth; j++) {
+        let tile = {
+          xPos: j * gridCellSize,
+          yPos: i * gridCellSize,
+        };
+
+        if (Math.random() > 0.2) {
+          tile = {
+            ...tile,
+            ...tileDefinition.grass
+          }
+        } else {
+          tile = {
+            ...tile,
+            ...tileDefinition.roadCrossingRound
+          }
+        }
+
+        tileDistribution.push(tile);
+      }
+    }
+
+    setMapLayout(tileDistribution);
+  }, [boardWidth, gridCellSize]);
+
+  return (
+    <Screen>
+      <Board cellCountPerDirection={boardWidth} gridCellSize={gridCellSize}>
+        {mapLayout &&
+          mapLayout.map((tile, i) => {
+            return (
+            <GridCell key={i} image={tile.img}>
+              {console.log(tile)}
+              x:{tile.xPos}
+              <br/>
+              y:{tile.yPos}
+            </GridCell>
+            )
+          })}
+      </Board>
+    </Screen>
+  );
+}
+
+const Screen = styled.div`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: hsla(55, 85%, 0%);
+  overflow: hidden;
+`;
+
+const Board = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(
+    ${(props) => props.cellCountPerDirection},
+    ${(props) => props.gridCellSize}px
+  );
+  grid-template-rows: repeat(
+    ${(props) => props.cellCountPerDirection},
+    ${(props) => props.gridCellSize}px
+  );
+  border-radius: 6px;
+  overflow: auto;
+`;
+
+const GridCell = styled.div`
+  background-image: url(${(props) => props.image});
+  background-position: center;
+  background-size: cover;
+  font-size: 14px;
+  outline: 1px solid hsla(0,0%,0%,0.1);
+  padding: 3px 5px;
+  color: hsla(0,0%,0%,0.6);
+`;
